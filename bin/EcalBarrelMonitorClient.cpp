1411b7cb2fd4217ada429b4c71dd36037bf6eeed
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2005-10-08 08:26:52 +0000

    *** empty log message ***

---
+++ bin/EcalBarrelMonitorClient.cpp
@@ -0,0 +1,16 @@
+
+#include "TROOT.h"
+#include "TFile.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  cout << endl;
+  cout << " *** Ecal Barrel Monitor Client ***" << endl;
+  cout << endl;
+
+  return 0;
+}
