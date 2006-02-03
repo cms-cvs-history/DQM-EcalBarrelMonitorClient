1c787fa311a30937ec21f7ffe979294290910ec8
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2006-02-03 11:12:23 +0000

    - first attempt to build a state-machine using available tools ...

---
+++ bin/stubs/EBMonitorClientStateMachine.cc
@@ -0,0 +1,100 @@
+/*
+ * \file EBMonitorClientStateMachine.cc
+ *
+ * $Date: 2006/02/03 09:39:06 $
+ * $Revision: 1.86 $
+ * \author G. Della Ricca
+ *
+*/
+
+#include "EBMonitorClientStateMachine.h"
+
+class EBMonitorClientStateMachine : public DQMBaseClient, public dqm::UpdateObserver{
+
+public:
+
+XDAQ_INSTANTIATOR();
+
+EBMonitorClientStateMachine(xdaq::ApplicationStub *s) : DQMBaseClient(s, "test"){ }
+
+// called by configureAction()
+void configure(){
+
+  edm::ParameterSet ps;
+
+  ps.addUntrackedParameter<string>("clientName", "test");
+
+  ps.addUntrackedParameter<string>("hostName", "localhost");
+  ps.addUntrackedParameter<int>("hostPort", 9090);
+
+//  ps.addUntrackedParameter<string>("outputFile", "EcalBarrelMonitorClient.root");
+
+//  ps.addUntrackedParameter<string>("dbName", "ecalh4db");
+//  ps.addUntrackedParameter<string>("dbHostName", "pccmsecdb.cern.ch");
+//  ps.addUntrackedParameter<string>("dbUserName", "test06");
+//  ps.addUntrackedParameter<string>("dbPassword", "oratest06");
+  ps.addUntrackedParameter<bool>("enableSubRun", true);
+
+  ps.addUntrackedParameter<string>("location", "H4");
+
+//  ps.addUntrackedParameter<string>("baseHtmlDir", ".");
+
+//  ps.addUntrackedParameter<bool>("collateSources", true);
+//  ps.addUntrackedParameter<bool>("cloneME", false);
+
+  ps.addUntrackedParameter<bool>("enableExit", false);
+
+//  ps.addUntrackedParameter<bool>("verbose", true);
+
+  ebmc_ = new EcalBarrelMonitorClient(ps, mui_);
+
+}
+
+// called by enableAction()
+void newRun(){
+
+  upd_->registerObserver(this);
+
+  ebmc_->beginJob();
+
+//  ebmc_->beginRun();
+
+}
+
+// called by haltAction()
+void endRun(){
+
+  ebmc_->endRun();
+
+}
+
+// called by ~DQMBaseClient()
+void finalize(){
+
+  ebmc_->endJob();
+
+  if ( ebmc_ ) delete ebmc_;
+
+}
+
+// called by Updater()
+void onUpdate() const {
+
+//  std::vector<std::string> uplist;
+//  mui_->getUpdatedContents(uplist);
+//  std::cout << "updated objects" << std::endl;
+//  for(unsigned i=0; i<uplist.size(); i++)
+//    std::cout << uplist[i] << std::endl;
+
+  ebmc_->analyze();
+
+}
+
+private:
+
+EcalBarrelMonitorClient* ebmc_;
+
+};
+
+XDAQ_INSTANTIATOR_IMPL(EBMonitorClientStateMachine)
+
---
+++ bin/stubs/EBMonitorClientStateMachine.h
@@ -0,0 +1,24 @@
+#ifndef EBMONITORCLIENTSTATEMACHINE_H
+#define EBMONITORCLIENTSTATEMACHINE_H
+
+/*
+ * \file EBMonitorClientStateMachine.h
+ *
+ * $Date: 2006/02/03 09:39:06 $
+ * $Revision: 1.34 $
+ * \author G. Della Ricca
+ *
+*/
+
+#include "DQMServices/Components/interface/DQMBaseClient.h"
+#include "DQMServices/Components/interface/Updater.h"
+#include "DQMServices/Components/interface/UpdateObserver.h"
+#include "DQMServices/Core/interface/MonitorUserInterface.h"
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include "DQM/EcalBarrelMonitorClient/interface/EcalBarrelMonitorClient.h"
+
+#endif
