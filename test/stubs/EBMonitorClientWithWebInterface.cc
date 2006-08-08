24578a070c3e89cd22c613ce6ec261965ae4c17a
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2006-08-08 09:13:05 +0000

    - add extra FU

--- test/stubs/EBMonitorClientWithWebInterface.cc
+++ test/stubs/EBMonitorClientWithWebInterface.cc
@@ -158,6 +158,7 @@ void EBMonitorClientWithWebInterface::configure()
   ps.addUntrackedParameter<bool>("enableMonitorDaemon", true);
 
   ps.addUntrackedParameter<string>("prefixME", "Collector/FU0/");
+//  ps.addUntrackedParameter<string>("prefixME", "Collector/FU1/");
 //  ps.addUntrackedParameter<string>("prefixME", "EvF/FU0/");
 
 //DQMBaseClient: ps.addUntrackedParameter<bool>("enableServer", true);
